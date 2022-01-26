b895b5b75665a012db6d2b71229f4d1084db0e06
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-01-26 07:33:01 -0800

    Client Server Project Init
    Initialized server.cpp, client.cpp, main.cpp, and CMAKE script. Set up Milestone 1 word document (shared via OneDrive).

---
+++ ClientServer/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file Test file for server.cpp and client.cpp
+ * @authors Phuc To, Narissa Tsuboi
+ * @date 2/6/22
+ * @version 1.0
+ *
+ */
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ ClientServer/server.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file Code for server socket programming
+ * @authors Phuc To, Narissa Tsuboi
+ * @date 2/6/22
+ * @version 1.0
+ * @reference https://www.geeksforgeeks.org/socket-programming-cc/
+ *
+ */
