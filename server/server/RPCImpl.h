849300ad4e80463b98af52261b37bdddb08e3639
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-02-24 18:12:00 -0800

    Create RPCImpl.h

---
+++ server/server/RPCImpl.h
@@ -0,0 +1,20 @@
+/**
+ @file  RPCImpl.h
+ @authors Phuc T, Narissa T, Kristen K
+ @date 3/10/22
+ @version 1.0
+ @reference https://www.geeksforgeeks.org/socket-programming-cc/
+ @reference https://www.gnu.org/software/libc/manual/html_node/Sockets.html
+ */
+
+
+#pragma once
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <vector>
+#include <iterator>
