3e6878d544e9d3a298db9806b2b13ab1b787c6c6
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-02-27 21:26:26 -0800

    Add Auth class to Server solution
    Added Auth.h and Auth.cpp to Server solution. Renamed all class files to have Upper Case letters. Renamed all VS solutions to have Upper Case letters.

---
+++ Server/Server/Auth.cpp
@@ -0,0 +1,37 @@
+/**
+ @file auth.cpp
+ @authors Phuc T, Narissa T, Kristen K
+ @date 3/10/22
+ @version 1.0
+ */
+
+#include "auth.h"
+#include <unordered_map>
+
+Auth::Auth()
+{
+
+}
+
+Auth::~Auth()
+{
+}
+
+bool Auth::UserLookup(const string username, const string password)
+{
+	string key = username + password;
+	bool isAdmin = false; 
+
+	// check if user already exists
+	if (this->userLookupMap.find(key) == userLookupMap.end()) {
+		cout << "New user, creating an account...\n";
+		this->userLookupMap.insert({key, true});
+		cout << "New user created. Logged in successfully\n";
+	}
+	else {
+		this->userLookupMap[key] = true;
+		cout << "User found. Logged in successfully\n";
+	}
+	return true;
+}
+
---
+++ Server/Server/Auth.h
@@ -0,0 +1,48 @@
+/**
+ @file auth.h
+ @authors Phuc T, Narissa T, Kristen K
+ @date 3/10/22
+ @version 1.0
+ */
+
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+class Auth
+{
+public:
+	/** Auth class constructor*/
+	Auth();
+
+	/** Auth class destructor*/
+	~Auth();
+
+	/**
+	UserLookup checks if there is a user name and password already saved to the map. 
+	If not, inits new user message to client, else, checks if admin priveleges should 
+	be on.
+	@param username
+	@param password
+	@return true for logged in
+	@pre user doesn't exist or is not logged in
+	@post user is logged in
+	*/
+	bool UserLookup(const string username, const string password);
+
+
+private: 
+	string username;
+	string password;
+
+	// key:usernamepassword; value:true/false for logged in 
+	unordered_map<string, bool> userLookupMap; 
+	// key:username, value:true/false
+	unordered_map<string, bool> adminLookupMap;  
+
+};
+
